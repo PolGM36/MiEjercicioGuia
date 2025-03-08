965b76e40758867a2f056de2974f7e63c5ed007e
unknown <pol.guasch.mesia@estudiantat.upc.edu>
2025-03-09 00:13:50 +0100

    Version con conexion desconexion

---
+++ Cliente/Cliente/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace Cliente
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Peticion = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.Peticion.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Peticion
+            // 
+            this.Peticion.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.Peticion.Controls.Add(this.label3);
+            this.Peticion.Controls.Add(this.textBox2);
+            this.Peticion.Controls.Add(this.radioButton3);
+            this.Peticion.Controls.Add(this.button1);
+            this.Peticion.Controls.Add(this.radioButton2);
+            this.Peticion.Controls.Add(this.radioButton1);
+            this.Peticion.Controls.Add(this.textBox1);
+            this.Peticion.Controls.Add(this.label2);
+            this.Peticion.Controls.Add(this.label1);
+            this.Peticion.Location = new System.Drawing.Point(214, 168);
+            this.Peticion.Name = "Peticion";
+            this.Peticion.Size = new System.Drawing.Size(1139, 630);
+            this.Peticion.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Gainsboro;
+            this.button1.Location = new System.Drawing.Point(520, 441);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(174, 51);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Enviar";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(433, 311);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(435, 36);
+            this.radioButton2.TabIndex = 8;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Dime la longitud de mi nombre";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(433, 247);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(408, 36);
+            this.radioButton1.TabIndex = 7;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Dime si mi nombre es bonito";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(433, 163);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(376, 38);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(254, 155);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(173, 46);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nombre:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(24, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(244, 69);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Petición";
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.radioButton3.Location = new System.Drawing.Point(433, 376);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(248, 36);
+            this.radioButton3.TabIndex = 10;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Dime si soy alto";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(156, 374);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(205, 38);
+            this.textBox2.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.900001F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(29, 372);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(115, 39);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Altura:";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Gainsboro;
+            this.button2.Location = new System.Drawing.Point(214, 82);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(174, 51);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Conectar";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Gainsboro;
+            this.button3.Location = new System.Drawing.Point(214, 842);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(217, 51);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Desconectar";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2150, 1132);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Peticion);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Peticion.ResumeLayout(false);
+            this.Peticion.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Peticion;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Servidor.c
@@ -0,0 +1,112 @@
+#include <unistd.h>
+#include <string.h> 
+#include <stdlib.h>
+#include <stdio.h> 
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+
+int main(int argc, char *argv[])
+{
+	
+int sock_conn, sock_listen, ret;
+struct sockaddr_in serv_adr;
+char peticion[512];
+char respuesta[512];
+
+if((sock_listen = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+{
+	printf("Error creant socket");
+}
+
+memset(&serv_adr, 0, sizeof(serv_adr));
+serv_adr.sin_family = AF_INET;
+serv_adr.sin_addr.s_addr = htonl(INADDR_ANY);
+serv_adr.sin_port = htons(9070);
+
+if(bind(sock_listen, (struct sockaddr *) &serv_adr, sizeof(serv_adr)) < 0)
+{
+	printf("Error al bind");
+}
+
+if(listen(sock_listen, 3) < 0)
+{
+	printf("Error en el listen");
+}
+
+int i;
+
+for(i=0;i<5;i++)
+{
+	printf("Escuchando\n");
+	sock_conn = accept(sock_listen, NULL, NULL);
+	printf ("He recibido conexion\n");
+	
+	int terminar = 0;
+	
+	while(terminar==0)
+	{
+		ret = read(sock_conn, peticion, sizeof(peticion));
+		printf("Recibido\n");
+		
+		peticion[ret]='\0';
+		
+		printf("Peticion: %s\n", peticion);
+		char nombre[20];
+		char *p = strtok(peticion, "/");
+		int codigo = atoi(p);
+		
+		if(codigo!=0)
+		{
+			p = strtok(NULL, "/");
+			strcpy (nombre,p);
+			printf("Codigo: %d, Nombre: %s\n, codigo, nombre");
+		}
+		
+		if(codigo ==0)
+		{
+			terminar = 1;
+		}		
+		else if(codigo ==1)
+		{
+			sprintf(respuesta, "%d,",strlen(nombre));
+		}
+		else if(codigo ==2) 
+		{
+			if((nombre[0]=='M') || (nombre[0]=='S'))
+			{
+				strcpy(respuesta,"SI");
+			}
+			else
+			{
+				strcpy(respuesta,"NO");
+			}
+		}
+		else
+		{
+			p = strtok(NULL, "/");
+			float altura = atof(p);
+			if(altura > 1.70)
+			{
+				sprintf(respuesta, "%s: eres alto", nombre);
+			}
+			else
+			{
+				sprintf(respuesta, "%s: eres bajo", nombre);
+			}
+		}
+		
+		if(codigo!=0)
+		{
+			printf("Respuesta: %s\n", respuesta);
+			write(sock_conn,respuesta, strlen(respuesta));
+		}
+		
+	}
+	
+	close(sock_conn);
+}
+	
+}	
+	
+	
